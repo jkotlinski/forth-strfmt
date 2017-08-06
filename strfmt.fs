f1a9b0e11b330aff719bb4b8b22747427607832c
Johan Kotlinski <kotlinski@gmail.com>
2017-08-06 19:00:28 +0200

    use src variable instead of loop

--- strfmt.fs
+++ strfmt.fs
@@ -11,19 +11,24 @@ swap 0 <# #s #> move-and-inc-dst ;
 : strfmt-s ( c-addr u dst -- dst )
 -rot move-and-inc-dst ;
 
+variable src
+0 value srcend
+
 \ Prints into buffer c-addr2 using the format string at c-addr1 u.
 \ caddr2 u3 is the resulting string.
 : strfmt ( c-addr1 u1 c-addr2 -- caddr2 u3 )
-dup >r -rot over + swap ?do
-i c@ '%' = if i 1+ c@ case
-'%' of '%' over c! 1+ 2 endof
-'c' of tuck c! 1+ 2 endof
-'n' of strfmt-n 2 endof
-'u' of strfmt-u 2 endof
-'s' of strfmt-s 2 endof
-'d' of i 2 + c@ case
+dup >r -rot over + to srcend src !
+begin src @ srcend < while
+src @ c@ '%' = if 1 src +! src @ c@ case
+'%' of '%' over c! 1+ endof
+'c' of tuck c! 1+ endof
+'n' of strfmt-n endof
+'u' of strfmt-u endof
+'s' of strfmt-s endof
+'d' of 1 src +! src @ c@ case
     'n' of strfmt-dn endof
     'u' of strfmt-du endof
-endcase 3 endof
-endcase else i c@ over c! 1+ 1 then
-+loop r> tuck - ;
+endcase endof
+endcase else src @ c@ over c! 1+ then
+1 src +! repeat
+r> tuck - ;
