df8c86db529823bc976e435c64a029fcf0749643
Johan Kotlinski <kotlinski@gmail.com>
2017-08-07 09:40:14 +0200

    Tidy up

--- printf.fs
+++ printf.fs
@@ -4,40 +4,27 @@ variable pad-char
 variable charbuf
 
 : pad-left ( dst c-addr u -- dst c-addr u )
-left-justify @ if exit then
-dup min-field-width @ < if
-min-field-width @ over - >r
-rot dup r@ pad-char @ fill r> + -rot then ;
+left-justify @ if exit then dup min-field-width @ < if
+min-field-width @ over - >r rot dup r@ pad-char @ fill r> + -rot then ;
 
 : pad-right ( dst u -- dst )
-tuck + swap left-justify @ if
-min-field-width @ swap - >r r@ 0> if
-dup r@ bl fill r@ + then rdrop
-else drop then ;
+tuck + swap left-justify @ if min-field-width @ swap - >r r@ 0> if
+dup r@ bl fill r@ + then rdrop else drop then ;
 
-: add-field ( dst c-addr u -- dst )
-pad-left >r over r@ move r> pad-right ;
-
-: sprintf-n ( n dst -- dst )
-swap dup s>d dabs <# #s rot sign #> add-field ;
-: sprintf-u ( u dst -- dst )
-swap 0 <# #s #> add-field ;
-: sprintf-dn ( d dst -- dst )
--rot tuck dabs <# #s rot sign #> add-field ;
-: sprintf-du ( du dst -- dst )
--rot <# #s #> add-field ;
+: add-field ( dst c-addr u -- dst ) pad-left >r over r@ move r> pad-right ;
+: sprintf-n ( n dst -- dst ) swap dup s>d dabs <# #s rot sign #> add-field ;
+: sprintf-u ( u dst -- dst ) swap 0 <# #s #> add-field ;
+: sprintf-dn ( d dst -- dst ) -rot tuck dabs <# #s rot sign #> add-field ;
+: sprintf-du ( du dst -- dst ) -rot <# #s #> add-field ;
 
 : parse-min-field-width ( src srcend -- src srcend )
 over c@ '-' = dup if rot 1+ -rot then left-justify !
 over c@ '0' = if swap 1+ swap '0' else bl then pad-char !
-base @ >r decimal
-over - 0 -rot 0 -rot
->number rot drop rot min-field-width !
-over + r> base ! ;
+base @ >r decimal over - 0 -rot 0 -rot >number
+rot drop rot min-field-width ! over + r> base ! ;
 
 : parse-cmdspec ( dst src srcend -- dst src srcend )
-swap 1+ swap parse-min-field-width
->r dup >r c@ case
+swap 1+ swap parse-min-field-width >r dup >r c@ case
 '%' of s" %" add-field endof
 'c' of swap charbuf c! charbuf 1 add-field endof
 'n' of sprintf-n endof
@@ -51,13 +38,8 @@ endcase endof endcase r> r> ;
 \ Prints n*x into buffer c-addr2 using the format string at c-addr1 u.
 \ caddr2 u3 is the resulting string.
 : sprintf ( n*x c-addr1 u1 c-addr2 -- caddr2 u3 )
-dup >r -rot over + \ dst src srcend
-begin 2dup < while
-over c@ '%' = if parse-cmdspec
-else -rot 2dup c@ swap c! -rot 1+ -rot then
-swap 1+ swap repeat
-2drop r> tuck - ;
+dup >r -rot over + begin 2dup < while over c@ '%' = if parse-cmdspec else
+-rot 2dup c@ swap c! -rot 1+ -rot then swap 1+ swap repeat 2drop r> tuck - ;
 
 \ Prints n*x using the format string at c-addr u.
-: printf ( n*x c-addr u -- )
-pad sprintf type ;
+: printf ( n*x c-addr u -- ) pad sprintf type ;
